f274f9850790ac132f7ac956b0c45eb21d4df49f
David Felley <david.felley@students.hevs.ch>
2018-04-30 11:09:06 +0200

    first commit

---
+++ src/Main/Main.java
@@ -0,0 +1,16 @@
+/*
+*Author David Felley
+*Created on 30 avr. 2018
+*For JaquetFelley
+*/
+
+package Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
