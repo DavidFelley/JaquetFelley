e50f0777a7e5328f176c2070b739769939a837ec
david <david.felley@students.hevs.ch.>
2018-05-12 13:48:32 +0200

    Création répertoire images, icones et fond. Ajout de l'heure toppanel

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,16 +7,14 @@
 package Main;
 
 public class Main {
-
 	
-	
-	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		
 		MainFrame smartphone = new MainFrame();
 		smartphone.setVisible(true);
-
+		
 	}
 
 }
--- src/Main/MainFrame.java
+++ src/Main/MainFrame.java
@@ -10,26 +10,33 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 
 import View.BackgroundImagePanel;
 
 public class MainFrame extends JFrame{
-	
-	
+		
 		// Panel principal
-		private BackgroundImagePanel backgroundPanel = new BackgroundImagePanel(new ImageIcon("images/wallpaper1.jpg"));
+		private BackgroundImagePanel backgroundPanel = new BackgroundImagePanel(new ImageIcon("images/Fond/wallpaper1.jpg"));
 		
 		// Panel de forme du smartphone
-		private BackgroundImagePanel formPanel = new BackgroundImagePanel(new ImageIcon("images/smartphone.png"));
+		private BackgroundImagePanel formPanel = new BackgroundImagePanel(new ImageIcon("images/Fond/smartphone.png"));
 		
 		// Gestion des panels
 		private CardLayout cardLayout = new CardLayout();
@@ -40,10 +47,22 @@ public class MainFrame extends JFrame{
 		
 		//Top Panel
 		private JPanel topPanel = new JPanel();
+		//Heure Top Panel
+		private JLabel heure = new JLabel();
+		final private DateFormat DATEFORMAT = new SimpleDateFormat("HH:mm");
+		private Timer timer = new Timer(0, new CurrentTime());
 	
-		public MainFrame() 
-		{
-		
+	public MainFrame() 
+	{
+		//Top Panel
+		topPanel.setOpaque(false);
+		topPanel.setPreferredSize(new Dimension(450, 50));
+		timer.start();
+		topPanel.add(heure);
+		heure.setForeground(Color.WHITE);
+		heure.setPreferredSize(new Dimension(57, 40));
+	    heure.setFont(new Font(null, Font.BOLD, 18));
+	    
 		//PARAMETRES DE LA FRAME
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(450,800);
@@ -68,12 +87,15 @@ public class MainFrame extends JFrame{
 		homePanel.setOpaque(false);
 		homePanel.setPreferredSize(new Dimension(450, 50));
 		
-		//Top Panel
-		topPanel.setOpaque(false);
-		topPanel.setPreferredSize(new Dimension(450, 50));
+	}
 		
-		
-		
-		
+	class CurrentTime implements ActionListener 
+	{
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			Calendar now = Calendar.getInstance();
+			heure.setText(DATEFORMAT.format(now.getTime()));
+		}
 	}
 }
