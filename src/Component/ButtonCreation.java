ec401030bd398cb2c2395fb59b76cf8998a77df4
val <valentin.jaquet@students.hevs.ch>
2018-05-12 16:54:25 +0200

    Création du homePanel GridBagLayout, ajout bouton power, test différents icônes

--- src/Component/ButtonCreation.java
+++ src/Component/ButtonCreation.java
@@ -7,35 +7,41 @@
 package Component;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.border.EmptyBorder;
 
 public class ButtonCreation extends JButton {
 	
 	public ButtonCreation() {
-		setLayout(new BorderLayout());  
+//		setLayout(new BorderLayout());  
 		
 	}
-	
+		
 	public ButtonCreation(String name, ImageIcon image) {
 		super(image);
 		
-		setLayout(new BorderLayout());
+//		setLayout(new BorderLayout());
 		setName(name);
 	}
 	
 	public ButtonCreation (ImageIcon image) {
 		super(image);
-		
-		setLayout(new BorderLayout());		
+		setPreferredSize(new Dimension (100,100)); // à supprimer
+		setBackground(new Color(200,0,0));
+//		setLayout(new BorderLayout());		
+		setFocusable(false);
+		setBorder(new EmptyBorder(0, 0, 0, 0));
+//		setContentAreaFilled(false);
 	}
 	
 	public ButtonCreation(ImageIcon image, int width, int height) {
 		super(image);
 		
-		setLayout(new BorderLayout());
+//		setLayout(new BorderLayout());
 		setPreferredSize(new Dimension(width, height));
 	}
 	
--- src/Main/MainFrame.java
+++ src/Main/MainFrame.java
@@ -13,9 +13,13 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -28,19 +32,13 @@ import javax.swing.Timer;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 
+import Component.ButtonCreation;
 import View.BackgroundImagePanel;
 
 public class MainFrame extends JFrame{
-	
-		//Top Panel
-		private JPanel topPanel = new JPanel();
-		//Heure Top Panel
-		private JLabel heure = new JLabel();
-		final private DateFormat DATEFORMAT = new SimpleDateFormat("HH:mm");
-		private Timer timer = new Timer(0, new CurrentTime());
 		
 		// Panel principal
-		private BackgroundImagePanel backgroundPanel = new BackgroundImagePanel(new ImageIcon("images/Fond/wallpaper1.jpg"));
+		private BackgroundImagePanel backgroundPanel = new BackgroundImagePanel(new ImageIcon("images/Fond/wallpaper.jpg"));
 		
 		// Panel de forme du smartphone
 		private BackgroundImagePanel formPanel = new BackgroundImagePanel(new ImageIcon("images/Fond/smartphone.png"));
@@ -49,9 +47,27 @@ public class MainFrame extends JFrame{
 		private CardLayout cardLayout = new CardLayout();
 		private JPanel contentPanel = new JPanel(cardLayout);
 		
-		//Panel Home(bottom)
+		//Panel Home
 		private JPanel homePanel = new JPanel();
 		
+		//Top Panel
+		private JPanel topPanel = new JPanel();
+		
+		//Heure Top Panel
+		private JLabel heure = new JLabel();
+		final private DateFormat DATEFORMAT = new SimpleDateFormat("HH:mm");
+		private Timer timer = new Timer(0, new CurrentTime());
+		
+//Panel applications
+private JPanel homepagePanel = new JPanel (new GridBagLayout());
+private GridBagConstraints c = new GridBagConstraints();
+		
+		
+//Boutons et applications
+private ButtonCreation offButton = new ButtonCreation(new ImageIcon("images/Icones/powerVide.png"));
+private ButtonCreation contactButton = new ButtonCreation(new ImageIcon("images/Icones/contact.png"));
+private ButtonCreation galleryButton = new ButtonCreation(new ImageIcon("images/Icones/gallery.png"));
+private ButtonCreation bourseButton = new ButtonCreation(new ImageIcon("images/Icones/bourse.png"));
 	
 	public MainFrame() 
 	{
@@ -87,8 +103,51 @@ public class MainFrame extends JFrame{
 		//Home Panel
 		homePanel.setOpaque(false);
 		homePanel.setPreferredSize(new Dimension(450, 50));
-				
 		
+//Homepage panel
+homepagePanel.setOpaque(false);
+backgroundPanel.add(homepagePanel, BorderLayout.CENTER);	
+
+//APPLICATIONS
+c.insets = new Insets(15,15,15,15);
+c.fill = GridBagConstraints.BOTH;
+c.weightx = 2;
+c.weighty = 2;
+c.ipady = c.anchor = GridBagConstraints.CENTER;
+
+
+//BOUTON OFF
+c.gridx = 0;
+c.gridy = 0;
+c.gridwidth = 1;
+c.gridheight = 1;
+homepagePanel.add(offButton,c);
+offButton.addActionListener(new ClickPowerOff());
+offButton.addMouseListener(new MouseBackground());
+
+
+c.gridx = 0;
+c.gridy = 1;
+c.gridwidth = 1;
+c.gridheight = 1;
+homepagePanel.add(contactButton,c);
+
+c.gridx = 1;
+c.gridy = 0;
+c.gridwidth = 1;
+c.gridheight = 1;
+homepagePanel.add(bourseButton,c);
+
+c.gridx = 1;
+c.gridy = 1;
+c.gridwidth = 1;
+c.gridheight = 1;
+homepagePanel.add(galleryButton,c);
+
+
+
+
+
 	}
 		
 	class CurrentTime implements ActionListener 
@@ -100,4 +159,57 @@ public class MainFrame extends JFrame{
 			heure.setText(DATEFORMAT.format(now.getTime()));
 		}
 	}
+	
+	class ClickPowerOff implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.exit(0);
+		}
+		
+	}
+	
+	class MouseBackground implements MouseListener{
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+	
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			offButton.setIcon(new ImageIcon("images/Icones/powerVide.png"));
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+		
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			offButton.setIcon(new ImageIcon("images/Icones/power.png"));
+			
+			
+		}
+		
+	}
+	
+	
+	
 }
