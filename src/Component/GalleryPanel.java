d0ad6dd7748d96ed1401f58998f37c4eb0ff83ed
david <david.felley@students.hevs.ch.>
2018-05-25 09:36:20 +0200

    Récupération de l'extension des fichiers img et numero img

--- src/Component/GalleryPanel.java
+++ src/Component/GalleryPanel.java
@@ -27,6 +27,9 @@ import Main.MainFrame;
 
 public class GalleryPanel extends JPanel{
 	
+	//Varia
+	private int numImg ;
+	
 	private CardLayout clGallery = new CardLayout();
 	private JPanel panelGallery = new JPanel(clGallery);
 
@@ -38,8 +41,8 @@ public class GalleryPanel extends JPanel{
 	//Ici on défini le type d'extension que le browser va accepter
 	private FileNameExtensionFilter ff = new FileNameExtensionFilter("Extension types : Images", "jpg", "png", "jpeg");
 	
-	public GalleryPanel () {
-		
+	public GalleryPanel () 
+	{		
 		//Paramètre principaux de la galerie (Format, et topMenu)
 		setLayout(new BorderLayout());
 		add(panelGallery);
@@ -50,14 +53,7 @@ public class GalleryPanel extends JPanel{
 		//Browser d'image
 		fc.setApproveButtonText("Ajouter");
 		fc.setAcceptAllFileFilterUsed(false);
-		fc.setFileFilter(ff);
-		
-	
-		
-		
-		
-		
-		
+		fc.setFileFilter(ff);		
 	}
 	
 	class addGalerie implements ActionListener 
@@ -79,6 +75,56 @@ public class GalleryPanel extends JPanel{
 			}
 		}
 	}
+	
+	
+//	public void saveToGalerie(File img)
+//	{
+//		setNumImg(numImg++);
+//		
+//		try
+//		{
+//			File outputfile = new File("image/Galerie/photo"+numImg+getExtension(img));
+//			ImageIO.write(newimg, "png", outputfile);
+//		}
+//		catch (IOException e) 
+//		{
+//			e.printStackTrace();
+//		}
+//	}
+	
+	//Permet de récupérer l'extension de l'image que l'on veut enregistrer.
+	private String getExtension(File img)
+	{
+		String imgExtension = img.getName();
+		
+		String fileName = img.getName();
+		if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+			return imgExtension.substring(imgExtension.lastIndexOf(".") + 1);
+		else
+			return "";
+	}
+
+	public void setNumImg(int numImg)
+	{
+		this.numImg = numImg;
+	}
+	
+//	//Récupérer le numéro de l'image enregistrée
+//		public int setNumImg()
+//		{
+//				File galerie = new File("images/Galerie");
+//				File[] f = galerie.listFiles();
+//				numImg = f.length;
+//				
+//				if("images/Galerie/photo"+numImg)
+//				
+//				return numImg ;
+//		}
+	
+	
+
+	
+	
 	
 	
 	
